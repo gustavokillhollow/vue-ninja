dcc3f249b83b2081538454cea537e28b417ec450
gustavo.moliveira <gustavo.kill09@gmail.com>
2019-05-07 11:53:53 -0300

    Vuejs 07 05

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 new Vue({
 	el: '#vue-app',
 	data:{
-		name:'Shaun',
-		job: 'Ninja'
-
+		age:25,
+		x: 0,
+		y: 0
 	},
 	methods:{
-		greet: function(time){
-			return 'Good ' + time +' '+this.name;
+		add: function(inc){
+			this.age+=inc;
+		},
+		subtract: function(dec){
+			this.age-=dec;
+		},
+		updateXY: function(event){
+			this.x = event.offsetX;
+			this.y = event.offsetY;
 		}
 
 	}
